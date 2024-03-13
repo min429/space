79d1f2b8c2924eda716abe1ac8de8fee59739bf6
min429 <lsm990429@gmail.com>
2024-03-13 11:09:25 +0900

    initial server setting

---
+++ odlm-server/src/main/java/com/project/odlmserver/OdlmServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.odlmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdlmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdlmServerApplication.class, args);
+	}
+
+}
---
+++ odlm-server/src/test/java/com/project/odlmserver/OdlmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.odlmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdlmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
