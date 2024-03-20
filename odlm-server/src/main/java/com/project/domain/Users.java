a04491595bda2bf39d14a9ae4790a4142bb55fa6
hoihoi0101 <114476214+hoihoi0101@users.noreply.github.com>
2024-03-20 14:38:05 +0900

    init_3

--- odlm-server/src/main/java/com/project/domain/Users.java
+++ odlm-server/src/main/java/com/project/domain/Users.java
@@ -2,19 +2,23 @@ package com.project.domain;
 
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
+import lombok.*;
 
 
 @Entity
 @Getter @Setter
+
 public class Users {
 
     @Id
     @Column(name = "student_id")
     private Long id;
 
+    public Users(){
+
+    }
+
+    @Column
     private String email;
     private String password;
     private String name;
--- odlm-server/src/main/java/com/project/repository/UsersRepository.java
+++ odlm-server/src/main/java/com/project/repository/UsersRepository.java
@@ -1,38 +1,12 @@
 package com.project.repository;
 
 import com.project.domain.Users;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class UsersRepository {
-
-    private final EntityManager em;
-
-
-    public void save(Users user){
-        em.persist(user);
-    }
-
-    public Users findOne(Long id) {
-        return em.find(Users.class,id);
-    }
-
-    public List<Users> findAll(){
-        List<Users> result = em.createQuery("select u from Users  u", Users.class)
-                .getResultList();
-
-        return result;
-    }
-
-    public List<Users> findByName(String name) {
-        return em.createQuery("select u from Users u where u.name = :name", Users.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
-
+public interface UsersRepository extends JpaRepository<Users , Long> {
+    List<Users> findByName(String name);
 }
