45ed1da5e12598dce3c4292de2fb62847878a756
min429 <lsm990429@gmail.com>
2024-03-25 23:19:55 +0900

    코드 리팩토링, build.gradle 의존성 추가

--- odlm-server/src/main/java/com/project/odlmserver/repository/BoardRepository.java
+++ odlm-server/src/main/java/com/project/odlmserver/repository/BoardRepository.java
@@ -1,18 +1,7 @@
 package com.project.odlmserver.repository;
 
-import com.project.odlmserver.domain.Board;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
-public class BoardRepository {
-
-    private final EntityManager em;
-
-    public void save(Board board){
-        em.persist(board);
-    }
-
+public interface BoardRepository {
 }
