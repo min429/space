5af12520493a7c291ff4cfae1b855be7bb617559
min429 <lsm990429@gmail.com>
2024-04-02 15:42:48 +0900

    FCM 푸시알림(경고) 기능 구현

---
+++ odlm-server/src/main/java/com/project/odlmserver/service/FCMService.java
@@ -0,0 +1,42 @@
+package com.project.odlmserver.service;
+
+import com.google.firebase.messaging.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FCMService {
+
+    private final UsersService usersService;
+
+    public void sendNotification(String userToken) {
+        Message message = Message.builder()
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setTtl(3600 * 1000) // 푸시알림을 fcm 서버에 한시간 보관
+                        .setPriority(AndroidConfig.Priority.NORMAL)
+                        .setNotification(AndroidNotification.builder()
+                                .setTitle("자리를 비우셨나요?")
+                                .setBody("10분 후 자동으로 자리가 반납됩니다.")
+                                .setIcon("stock_ticker_update")
+                                .setColor("#f45342")
+                                .build())
+                        .build())
+                .setToken(userToken)
+                .build();
+
+        try{
+            String response = FirebaseMessaging.getInstance().send(message);
+            log.debug("response: {}", response);
+        }
+        catch(FirebaseMessagingException e){
+            log.error("error: {}", e.getMessage());
+        }
+    }
+
+    public void registerToken(Long userId, String token) {
+        usersService.updateToken(userId, token);
+    }
+}
