45fb6db13adf3569f0feac43ad74fc91d06b6503
hoihoi0101 <114476214+hoihoi0101@users.noreply.github.com>
2024-04-11 12:16:13 +0900

    test

--- odlm_app/test/widget_test.dart
+++ odlm_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:odlm_app/main.dart';
+import 'package:odlm_app/main_ckh.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
