cb3ce9127d991672118c34c9463b18546c37f0f2
min429 <lsm990429@gmail.com>
2024-05-29 00:05:43 +0900

    mypage 수정

--- odlm_app/lib/mypage.dart
+++ odlm_app/lib/mypage.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutterflow_ui/flutterflow_ui.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
-import 'package:fl_chart/fl_chart.dart';
 
 import 'globals.dart';
 
@@ -19,23 +18,10 @@ class _MypageWidgetState extends State<MypageWidget> {
   String dailyStudyTime = '0분';
   String monthlyStudyTime = '0시간 0분';
 
-  List<BarChartGroupData> barChartData = [
-    BarChartGroupData(
-      x: 0,
-      barRods: [
-        BarChartRodData(
-          toY: 0,
-          color: Colors.blue,
-        ),
-      ],
-    ),
-  ]; // 기본적으로 비어있지 않은 상태로 설정
-
   @override
   void initState() {
     super.initState();
     _loadStudyTimes();
-    _loadBarChartData();
   }
 
   @override
@@ -62,20 +48,6 @@ class _MypageWidgetState extends State<MypageWidget> {
     } catch (e) {
       print('Failed to load study times: $e');
     }
-  }
-
-  Future<void> _loadBarChartData() async {
-    // 여기에 차트 데이터를 로드하는 코드를 추가합니다.
-    // 예제 데이터로 설정
-    setState(() {
-      barChartData = [
-        BarChartGroupData(x: 6, barRods: [BarChartRodData(toY: 5, color: Colors.lightBlueAccent)]),
-        BarChartGroupData(x: 7, barRods: [BarChartRodData(toY: 10, color: Colors.lightBlueAccent)]),
-        BarChartGroupData(x: 8, barRods: [BarChartRodData(toY: 15, color: Colors.lightBlueAccent)]),
-        BarChartGroupData(x: 9, barRods: [BarChartRodData(toY: 20, color: Colors.lightBlueAccent)]),
-        BarChartGroupData(x: 10, barRods: [BarChartRodData(toY: 25, color: Colors.lightBlueAccent)]),
-      ];
-    });
   }
 
   Future<String> _fetchDailyStudyTime(int day) async {
@@ -138,44 +110,106 @@ class _MypageWidgetState extends State<MypageWidget> {
         ),
         body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
+            scrollDirection: Axis.horizontal, // 가로 스크롤 가능
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start, // 위쪽 정렬
               children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start, // 위쪽 정렬
-                  children: [
-                    // 첫 번째 컨테이너: 오늘의 학습시간
-                    Expanded(
-                      child: Container(
-                        height: 160,
+                // 첫 번째 컨테이너: 오늘의 학습시간
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: 160,
+                  color: FlutterFlowTheme.of(context).primaryBackground,
+                  child: Column(
+                    children: [
+                      // 오늘의 학습시간 타이틀
+                      Container(
+                        height: 70,
                         color: FlutterFlowTheme.of(context).primaryBackground,
-                        child: Column(
-                          children: [
-                            // 오늘의 학습시간 타이틀
-                            Container(
-                              height: 70,
-                              color: FlutterFlowTheme.of(context).primaryBackground,
-                              alignment: Alignment.center,
-                              child: Opacity(
-                                opacity: 0.7,
-                                child: Text(
-                                  '오늘의 학습시간',
-                                  style: FlutterFlowTheme.of(context)
-                                      .bodyMedium
-                                      .override(
-                                    fontFamily: 'Readex Pro',
-                                  ),
-                                ),
-                              ),
+                        alignment: Alignment.center,
+                        child: Opacity(
+                          opacity: 0.7,
+                          child: Text(
+                            '오늘의 학습시간',
+                            style: FlutterFlowTheme.of(context)
+                                .bodyMedium
+                                .override(
+                              fontFamily: 'Readex Pro',
                             ),
-                            // 오늘의 학습시간 값
-                            Container(
-                              height: 90,
-                              color: FlutterFlowTheme.of(context).primaryBackground,
-                              alignment: Alignment.center,
+                          ),
+                        ),
+                      ),
+                      // 오늘의 학습시간 값
+                      Container(
+                        height: 90,
+                        color: FlutterFlowTheme.of(context).primaryBackground,
+                        alignment: Alignment.center,
+                        child: Opacity(
+                          opacity: 0.7,
+                          child: Text(
+                            dailyStudyTime,
+                            style: FlutterFlowTheme.of(context)
+                                .bodyMedium
+                                .override(
+                              fontFamily: 'Readex Pro',
+                              fontSize: 23,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // 구분선
+                Padding(
+                  padding: const EdgeInsets.only(top: 10), // 위쪽 패딩 추가
+                  child: Opacity(
+                    opacity: 0.5,
+                    child: Container(
+                      width: 1,
+                      height: 120,
+                      color: FlutterFlowTheme.of(context).primaryText,
+                    ),
+                  ),
+                ),
+                // 두 번째 컨테이너: 이달의 학습시간
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: 160,
+                  color: FlutterFlowTheme.of(context).primaryBackground,
+                  child: Column(
+                    children: [
+                      // 이달의 학습시간 타이틀
+                      Container(
+                        height: 70,
+                        color: FlutterFlowTheme.of(context).primaryBackground,
+                        alignment: Alignment.center,
+                        child: Opacity(
+                          opacity: 0.7,
+                          child: Text(
+                            '이달의 학습시간',
+                            style: FlutterFlowTheme.of(context)
+                                .bodyMedium
+                                .override(
+                              fontFamily: 'Readex Pro',
+                            ),
+                          ),
+                        ),
+                      ),
+                      // 이달의 학습시간 값
+                      Container(
+                        height: 90,
+                        color: FlutterFlowTheme.of(context).primaryBackground,
+                        alignment: Alignment.center,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // 이달의 학습시간 값
+                            Flexible(
                               child: Opacity(
                                 opacity: 0.7,
                                 child: Text(
-                                  dailyStudyTime,
+                                  monthlyStudyTime,
                                   style: FlutterFlowTheme.of(context)
                                       .bodyMedium
                                       .override(
@@ -189,134 +223,7 @@ class _MypageWidgetState extends State<MypageWidget> {
                           ],
                         ),
                       ),
-                    ),
-                    // 구분선
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10), // 위쪽 패딩 추가
-                      child: Opacity(
-                        opacity: 0.5,
-                        child: Container(
-                          width: 1,
-                          height: 120,
-                          color: FlutterFlowTheme.of(context).primaryText,
-                        ),
-                      ),
-                    ),
-                    // 두 번째 컨테이너: 이달의 학습시간
-                    Expanded(
-                      child: Container(
-                        height: 160,
-                        color: FlutterFlowTheme.of(context).primaryBackground,
-                        child: Column(
-                          children: [
-                            // 이달의 학습시간 타이틀
-                            Container(
-                              height: 70,
-                              color: FlutterFlowTheme.of(context).primaryBackground,
-                              alignment: Alignment.center,
-                              child: Opacity(
-                                opacity: 0.7,
-                                child: Text(
-                                  '이달의 학습시간',
-                                  style: FlutterFlowTheme.of(context)
-                                      .bodyMedium
-                                      .override(
-                                    fontFamily: 'Readex Pro',
-                                  ),
-                                ),
-                              ),
-                            ),
-                            // 이달의 학습시간 값
-                            Container(
-                              height: 90,
-                              color: FlutterFlowTheme.of(context).primaryBackground,
-                              alignment: Alignment.center,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  // 이달의 학습시간 값
-                                  Flexible(
-                                    child: Opacity(
-                                      opacity: 0.7,
-                                      child: Text(
-                                        monthlyStudyTime,
-                                        style: FlutterFlowTheme.of(context)
-                                            .bodyMedium
-                                            .override(
-                                          fontFamily: 'Readex Pro',
-                                          fontSize: 23,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                // 막대 그래프 추가
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: BarChart(
-                    BarChartData(
-                      barGroups: barChartData,
-                      titlesData: FlTitlesData(
-                        bottomTitles: AxisTitles(
-                          sideTitles: SideTitles(
-                            showTitles: true,
-                            getTitlesWidget: (double value, TitleMeta meta) {
-                              const style = TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14,
-                              );
-                              Widget text;
-                              switch (value.toInt()) {
-                                case 6:
-                                  text = const Text('6월', style: style);
-                                  break;
-                                case 7:
-                                  text = const Text('7월', style: style);
-                                  break;
-                                case 8:
-                                  text = const Text('8월', style: style);
-                                  break;
-                                case 9:
-                                  text = const Text('9월', style: style);
-                                  break;
-                                case 10:
-                                  text = const Text('10월', style: style);
-                                  break;
-                                default:
-                                  text = const Text('', style: style);
-                                  break;
-                              }
-                              return SideTitleWidget(
-                                axisSide: meta.axisSide,
-                                child: text,
-                              );
-                            },
-                          ),
-                        ),
-                        leftTitles: AxisTitles(
-                          sideTitles: SideTitles(showTitles: false),
-                        ),
-                      ),
-                      borderData: FlBorderData(
-                        show: false,
-                      ),
-                      barTouchData: BarTouchData(
-                        touchTooltipData: BarTouchTooltipData(
-                          tooltipBgColor: Colors.blueAccent,
-                        ),
-                      ),
-                    ),
+                    ],
                   ),
                 ),
               ],
